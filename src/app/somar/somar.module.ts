008285aa638973c6eb48d3deaa6955eb199a9a68
MattheusCosta <matheuscosta1921@gmail.com>
2019-09-30 14:33:33 -0300

    componente do modulo Somar

--- src/app/somar/somar.module.ts
+++ src/app/somar/somar.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SomarComponent } from './components/somar';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SomarComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SomarComponent
   ]
 })
+
 export class SomarModule { }
